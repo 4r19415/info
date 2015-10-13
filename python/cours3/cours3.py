cc026745cf60f533633cff08191c0636bb01d6ad
4r19415 <justin.bezieau@gmx.com>
2015-10-13 07:35:28 +0200

    cours3

--- python/cours3/cours3.py
+++ python/cours3/cours3.py
@@ -1,4 +1,15 @@
-#!//usr/bin/python
+#!//home/lui/anaconda/bin/python
 #-*- coding:utf-8 -*-
 
+#regression lineaire
+
+
+from random import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.arange(10)
+
+plt.scatter(x, 3*x+2) #/!\ 3*x fonctionne parce que x est un numpy array
+plt.show()
 
---
+++ python/cours3/cours3.py~
@@ -0,0 +1,18 @@
+#!//usr/bin/python
+#-*- coding:utf-8 -*-
+
+def moyenne(v):
+    l=0.00
+    i=0
+    while i < len(v):
+        l=l+v[i]
+        i=i+1
+    return(l/i)
+
+
+a=range(10)
+b=range(100)
+c=[5,10]
+print(moyenne(a))
+print(moyenne(b))
+print(moyenne(c))
